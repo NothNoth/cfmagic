4c60a5c38fef39c048d6709c6fcb2a56ebd2c345
bgi <bgi@suneris.com>
2019-02-13 15:30:54 +0100

    More docs

--- README.md
+++ README.md
@@ -5,7 +5,7 @@ Clang is cool and Clang-Format is really cool, so adding genetic algorithms make
 ## Why ?
 
 Clang-Format is a great tool when you want to make sure your team uses the proper indent conventions so that your code base looks homogenous.
-Unfortunately, you will soon realize that there are plenty of clang-format configuration parameters, which usually will interact with each other.
+Unfortunately, you will soon realize that there are plenty of clang-format configuration parameters, which will frequently interact with each other.
 
 You usually end up changing randomly some settings that might fix your problem for several hours.
 
@@ -48,6 +48,40 @@ __Run:__
 
 You may want to adapt:
 
-  - `<clang format path>`: (ex.: usr/bin/clang-format-6.0) the path of your locally installed clang-format binary
+  - `<clang format path>`: (ex.: /usr/bin/clang-format-6.0) the path of your locally installed clang-format binary
   - `<population size>`: (ex.: 20) the size of the population (ie.: the number of randomly generated config files). A number between 10 and 100 is nice.
   - `<mutation rate>`: (ex: 14) the mutation rate. A number between 1 and 20 is nice
+
+## Credits
+
+The __configs.json__ file has been taken from [https://github.com/zed0/clang-format-configurator](https://github.com/zed0/clang-format-configurator), all credits to him.
+
+## FAQ
+
+__How long does it take to find a nice .clang-format configuration file matching my crazy indentation style?__
+
+A few minutes is usually enough.
+
+__Will cfmagic always find the ultimate .clang-format configuration file?__
+
+No, simply because all possibilities are not managed by clang-format. cfmagic will find a "close" solution.
+
+In addition to this, genetic algorithm do not necessarily find the best possible solution.
+
+__What about tuning population size and mutation rate parameters?__
+
+I find out that using a large population (ex. 100) seems to be counter productive.
+Mutation rate can be tuned, but adding too much randomness (> 20) doesn't make any sense.
+
+__I found your mutation boost secret feature!__
+
+After a certain number of generations (ie. iterations), you may end up with a set of individuals (ie. clang-format configs) very close to each other.
+At this point, you will want to renew things by adding more randomness.
+
+In order to do so, we compute the standard deviation of the scores for every generation. If too low, mutationRate is doubled for the next generation.
+
+__I see "floating point exceptions" errors__
+
+Funily enough, cfmagic also acts as a clang-format fuzzer!
+So "yes", some versions of clang-format may crash: this is handled by cfmagic and has no impact on the results (you may safely ignore these errors).
+
---
+++ cfmagic.go
@@ -0,0 +1,109 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"math"
+	"os"
+	"os/signal"
+	"strconv"
+	"syscall"
+)
+
+// JSON taken from https://zed0.co.uk/clang-format-configurator/doc/
+
+const configs_file = "configs.json"
+const minStdDevForMutationBoost = 5.0
+
+type ConfigEntry struct {
+	Type    string   `json:"type"`
+	Doc     string   `json:"doc"`
+	Options []string `json:"options"`
+}
+
+func main() {
+	var clangPath string
+	var perfectSource string
+	if len(os.Args) != 5 {
+		fmt.Println("Usage: " + os.Args[0] + " <clang format binary> <perfect source code> <population size> <mutation rate>")
+		fmt.Println("Example: " + os.Args[0] + "/usr/bin/clang-format-6.0 perfect.c 20 4")
+		return
+	}
+	clangPath = os.Args[1]
+	perfectSource = os.Args[2]
+	pop, err := strconv.Atoi(os.Args[3])
+	if err != nil {
+		fmt.Println("Error : invalid population size (default: 100)")
+		return
+	}
+	mut, err := strconv.Atoi(os.Args[4])
+	if err != nil {
+		fmt.Println("Error : invalid mutation rate (default: 4)")
+		return
+	}
+
+	//fmt.Println(entries)
+	entries, err := loadConfig(clangPath)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	cfmagic(clangPath, entries, perfectSource, uint32(pop), uint32(mut))
+}
+
+func cfmagic(clangPath string, configEntries map[string]*ConfigEntry, perfectSource string, populationSize uint32, mutationRate uint32) {
+
+	pop := genPopulation(populationSize, configEntries)
+	fmt.Println("Population ready")
+	fmt.Printf("Population size : %d | Mutation rate %d %%\n", populationSize, mutationRate)
+
+	done := false
+	sigc := make(chan os.Signal, 1)
+	signal.Notify(sigc,
+		syscall.SIGHUP,
+		syscall.SIGINT,
+		syscall.SIGTERM,
+		syscall.SIGQUIT)
+	go func() {
+		s := <-sigc
+		fmt.Println("Received signal " + s.String())
+		done = true
+	}()
+
+	for {
+		pop.generation++
+		//Eval everyone
+		for i := 0; i < len(pop.population); i++ {
+			err := pop.population[i].UpdateScore(clangPath, perfectSource)
+			if err != nil {
+				fmt.Println(err)
+				//return
+				pop.population[i].score = uint32(math.MaxUint32)
+			}
+		}
+
+		//Sort
+		pop.sort()
+		stdDev := pop.getStdDev(len(pop.population) / 2)
+		fmt.Printf("Best score for generation %d: %d (stdDev: %f)\n", pop.generation, pop.population[0].score, stdDev)
+
+		//Boost mutations if top population is too homogenous
+		if stdDev < minStdDevForMutationBoost {
+			//Mix
+			pop.mix(2*mutationRate, configEntries)
+
+		} else {
+			//Mix
+			pop.mix(mutationRate, configEntries)
+		}
+
+		if done == true {
+			break
+		}
+	}
+
+	fmt.Printf("Best configuration has score: %d\n", pop.population[0].score)
+	ioutil.WriteFile(".clang-format", []byte(pop.population[0].toClangFormatConfigFile()), os.ModePerm)
+	fmt.Println("Written to .clang-format")
+}
